a1766fd6eb7cd6fd12a00f4d13132cdbc6c16276
Bastian1110 <chebimora02@gmail.com>
2023-05-29 23:50:32 -0600

    Backend API routes completed

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from quiz_app import MongoManager, User
+from quiz_app import MongoManager, User, Question, Quiz
 
 app = Flask("Quiz App Server")
 CORS(app)
 
 my_manager = MongoManager("mongodb://localhost:27017", "quizapp")
-my_user = User(my_manager)
+user_controller = User(my_manager)
+question_controller = Question(my_manager)
+quiz_controller = Quiz(my_manager)
 
 
 @app.route("/")
@@ -14,14 +16,14 @@ def ping():
     return jsonify({"message": "Hello from QuizApp"}), 200
 
 
-@app.route("/register", methods=["POST"])
+@app.route("/user/register", methods=["POST"])
 def register():
     username = request.values.get("username")
     password = request.values.get("password")
     if not username or not password:
         return jsonify({"message": "Missing values"}), 400
     try:
-        message, result = my_user.create_user(username, password)
+        message, result = user_controller.create_user(username, password)
         if result == 0:
             return jsonify({"message": message}), 200
         return jsonify({"message": message}), 400
@@ -30,35 +32,100 @@ def register():
         return jsonify({"message": "Error creating the user"}), 500
 
 
-@app.route("/login", methods=["POST"])
+@app.route("/user/login", methods=["POST"])
 def login():
     username = request.values.get("username")
     password = request.values.get("password")
     if not username or not password:
         return jsonify({"message": "Missing values"}), 400
     try:
-        message, result, token = my_user.validate_user(username, password)
+        message, result, token, user_id = user_controller.validate_user(
+            username, password
+        )
         if result == 0:
-            return jsonify({"message": message, "token": token}), 200
+            return (
+                jsonify({"message": message, "token": token, "user_id": user_id}),
+                200,
+            )
         return jsonify({"message": message}), 400
     except Exception as e:
         print(e)
         return jsonify({"message": "Error validating the user"}), 500
 
 
-@app.route("/updateScore", methods=["POST"])
+@app.route("/user/updateScore", methods=["POST"])
 def handleScore():
     score = request.values.get("score")
     token = request.values.get("token")
     if not score or not token:
         return jsonify({"message": "Missing values"}), 400
     try:
-        message, result, new_score = my_user.update_score(token, score)
+        message, result, new_score = user_controller.update_score(token, score)
         if result == 0:
             return jsonify({"message": message, "new_score": new_score}), 200
         return jsonify({"message": message}), 400
     except:
         return jsonify({"message": "Error updating score"}), 500
+
+
+@app.route("/user/getTop", methods=["GET"])
+def getTop():
+    return jsonify({"message": "Success", "top": user_controller.get_top()})
+
+
+@app.route("/question/createQuestion", methods=["POST"])
+def handleCreateQuestion():
+    data = request.get_json()
+    try:
+        category = data["category"]
+        question = data["question"]
+        answers = data["answers"]
+        correct_index = data["correct_index"]
+        message, result = question_controller.create_question(
+            category, question, answers, correct_index
+        )
+        if result == 0:
+            return jsonify({"message": message}), 200
+        return jsonify({"message": message}), 500
+    except:
+        return jsonify({"message": "Missing value in the request"}), 400
+
+
+@app.route("/question/createQuiz", methods=["POST"])
+def getQuiz():
+    n = int(request.values.get("questions"))
+    category = request.values.get("category")
+    if not n:
+        n = 10
+    if not category:
+        category = "Random"
+    try:
+        message, error, questions = question_controller.generate_quiz(n, category)
+        return jsonify({"message": message, "questions": questions}), 200
+    except:
+        return jsonify({"message": "Error generating quiz"}), 500
+
+
+@app.route("/quiz/getQuestion", methods=["POST"])
+def handleGetQuestion():
+    question_id = request.values.get("id")
+    if not question_id:
+        return jsonify({"message": "Missing values"}), 400
+    message, error, result = quiz_controller.getQuestion(question_id)
+    if error == 0:
+        return jsonify({"message": message, "question": result}), 200
+    return jsonify({"message": message, "question": result}), 400
+
+
+@app.route("/quiz/answerQuestion", methods=["POST"])
+def answer():
+    question_id = request.values.get("id")
+    user_id = request.values.get("user_id")
+    answer = request.values.get("answer")
+    if not answer or not question_id or not user_id:
+        return jsonify({"message": "Missing values"}), 400
+    message, result = quiz_controller.answerQuestion(user_id, question_id, int(answer))
+    return jsonify({"message": message, "result": result}), 200
 
 
 if __name__ == "__main__":
--- backend/quiz_app.py
+++ backend/quiz_app.py
@@ -2,6 +2,8 @@ from pymongo import MongoClient
 from random import randint
 from hashlib import sha256
 from jwt import encode, decode
+from bson import ObjectId
+from random import sample
 
 
 class MongoManager:
@@ -38,17 +40,88 @@ class User:
                     "Success",
                     0,
                     encode({"user": document}, "Chikimiau"),
+                    document["_id"],
                 ]
-        return ["Bad username or password", 1, None]
+        return ["Bad username or password", 1, None, None]
+
+    def get_top(self):
+        result = list(self.collection.find().sort("score", -1).limit(5))
+        for doc in result:
+            doc["_id"] = str(doc["_id"])
+        return result
 
     def update_score(self, token, score):
         data = decode(token, "Chikimiau", algorithms=["HS256"])["user"]
         try:
-            print(data["_id"])
-            result = self.collection.find({"_id": data["_id"]})
-            for i in result:
-                print("miau", i)
+            result = self.collection.update_one(
+                {"_id": ObjectId(data["_id"])}, {"$inc": {"score": int(score)}}
+            )
+            print(result.modified_count)
             return ["Success", 0, score]
         except Exception as e:
             print("here ", e)
             return ["Bad token", 1, None]
+
+
+class Question:
+    def __init__(self, db_manager):
+        self.collection = db_manager.db_client["question"]
+
+    def create_question(self, category, question, answers, correct_index):
+        try:
+            self.collection.insert_one(
+                {
+                    "category": category,
+                    "question": question,
+                    "answers": answers,
+                    "correct": correct_index,
+                }
+            )
+            return ["Success", 0]
+        except:
+            return ["Error", 1]
+
+    def generate_quiz(self, n_question, catrgory="Random"):
+        quiz = []
+        if catrgory == "Random":
+            all_documents = list(self.collection.find({}, {"_id": 1}))
+        else:
+            all_documents = list(
+                self.collection.find({"category": catrgory}, {"_id": 1})
+            )
+        all_documents = [str(doc["_id"]) for doc in all_documents]
+        shuffled_documents = sample(all_documents, len(all_documents))
+        selected_documents = shuffled_documents[:n_question]
+        return ["Success", 0, selected_documents]
+
+
+class Quiz:
+    def __init__(self, db_manager):
+        self.questions = db_manager.db_client["question"]
+        self.user = db_manager.db_client["user"]
+
+    def getQuestion(self, question_id):
+        try:
+            result = list(
+                self.questions.find(
+                    {"_id": ObjectId(question_id)},
+                    {"question": 1, "answers": 1},
+                )
+            )[0]
+            result["_id"] = str(result["_id"])
+            return ["Success", 0, result]
+        except:
+            return ["Question dosent exist", 1, None]
+
+    def answerQuestion(self, user_id, question_id, answer):
+        result = list(
+            self.questions.find({"_id": ObjectId(question_id), "correct": answer})
+        )
+        if result:
+            points = 10 + randint(0, 8)
+            self.user.update_one(
+                {"_id": ObjectId(user_id)}, {"$inc": {"score": points}}
+            )
+            return ["Respuesta correcta", 0]
+        else:
+            return ["Respuesta incorrecta", 1]
